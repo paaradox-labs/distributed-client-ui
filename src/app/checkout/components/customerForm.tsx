fa4e79709549388262dfb4731650bf3174604a48
Mr. Paradox <avyas8927@gmail.com>
2026-06-22 11:03:23 +0530

    add: calculate order summary

--- src/app/checkout/components/customerForm.tsx
+++ src/app/checkout/components/customerForm.tsx
@@ -15,6 +15,7 @@ import { z } from 'zod/v4';
 import { standardSchemaResolver } from '@hookform/resolvers/standard-schema';
 import { useForm } from 'react-hook-form';
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import OrderSummary from './orderSummary';
 
 const formSchema = z.object({
     address: z.string({
@@ -172,47 +173,7 @@ onValueChange={field.onChange}
                     </div>
                 </CardContent>
             </Card>
-            <Card className="w-2/5 border-none h-auto self-start">
-                <CardHeader>
-                    <CardTitle>Order summary</CardTitle>
-                </CardHeader>
-                <CardContent className="grid gap-4 pt-6">
-                    <div className="flex items-center justify-between">
-                        <span>Subtotal</span>
-                        <span className="font-bold">₹8130</span>
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <span>Taxes</span>
-                        <span className="font-bold">₹82</span>
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <span>Delivery charges</span>
-                        <span className="font-bold">₹100</span>
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <span>Discount</span>
-                        <span className="font-bold">₹0</span>
-                    </div>
-                    <hr />
-                    <div className="flex items-center justify-between">
-                        <span className="font-bold">Order total</span>
-                        <span className="font-bold">₹8300</span>
-                    </div>
-                    <div className="flex items-center gap-4">
-                        <Input
-                            id="fname"
-                            type="text"
-                            className="w-full"
-                            placeholder="Coupon code"
-                        />
-                        <Button variant={'outline'}>Apply</Button>
-                    </div>
-
-                    <div className="text-right mt-6">
-                        <Button>Place order</Button>
-                    </div>
-                </CardContent>
-            </Card>
+            <OrderSummary />
         </div>
             </form>
         </Form>
