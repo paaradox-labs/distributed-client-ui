afe55a4a4d12a4866988395c31c53abf7634ef08
Mr. Paradox <avyas8927@gmail.com>
2026-07-01 16:22:51 +0530

    fix: make success page dynamic

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -2,52 +2,84 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
-import { ArrowLeft, CheckCircle2, LayoutDashboard, Store } from 'lucide-react';
+import { ArrowLeft, CheckCircle2, CircleX, LayoutDashboard, Store } from 'lucide-react';
 import Link from 'next/link';
 
-const Payment = () => {
+const Payment = async({searchParams}: {searchParams:  Promise<{success: string; orderId: string}>}) => {
+
+    const params = await searchParams
+    const isOrderSuccess = params.success === "true"
     return (
         <div className="flex flex-col items-center gap-4 w-full mt-16 md:mt-32 px-4 md:px-0">
-            <CheckCircle2 size={80} className="text-green-500" />
-            <h1 className="text-2xl font-bold mt-2 text-center">Order placed successfully.</h1>
-            <p className="text-base font-semibold -mt-2">Thank you for your order.</p>
+            {isOrderSuccess ? (
+                <>
+                    <CheckCircle2 size={80} className="text-green-500" />
+                    <h1 className="text-2xl font-bold mt-2 text-center">
+                        Order placed successfully.
+                    </h1>
+                    <p className="text-base font-semibold -mt-2">Thank you for your order.</p>
+                </>
+            ) : (
+                <>
+                    <CircleX size={80} className="text-red-500" />
+                    <h1 className="text-2xl font-bold mt-2 text-center">
+                        Payment has been failed.
+                    </h1>
+                    <p className="text-base font-semibold -mt-2">Please try again.</p>
+                </>
+            )}
 
-            <Card className="w-full max-w-lg mt-6">
-                <CardHeader className="p-4">
-                    <CardTitle className="flex flex-col sm:flex-row items-start text-lg justify-between gap-2 sm:gap-12">
-                        <div className="flex item-center gap-3">
-                            <Store size={35} className="text-primary" />
-                            <span>Your order information</span>
+            {isOrderSuccess && (
+                <Card className="mt-6 w-full max-w-lg">
+                    <CardHeader className="p-4">
+                        <CardTitle className="flex items-start text-lg justify-between gap-12">
+                            <div className="flex item-center gap-3">
+                                <Store size={35} className="text-primary" />
+                                <span>Your order information</span>
+                            </div>
+                            <Badge className="text-base px-4" variant={'secondary'}>
+                                Confirmed
+                            </Badge>
+                        </CardTitle>
+                    </CardHeader>
+                    <Separator />
+                    <CardContent className="pt-6">
+                        <div className="flex items-center gap-2">
+                            <LayoutDashboard size={20} />
+                            <h2 className="text-base font-medium">Order reference: </h2>
+                            <Link
+                                href={`/order-status/${params.orderId}`}
+                                className="underline">
+                                {params.orderId}
+                            </Link>
                         </div>
-                        <Badge className="text-base px-4" variant={'secondary'}>
-                            Confirmed
-                        </Badge>
-                    </CardTitle>
-                </CardHeader>
-                <Separator />
-                <CardContent className="pt-6">
-                    <div className="flex items-center gap-2">
-                        <LayoutDashboard size={20} />
-                        <h2 className="text-base font-medium">Order reference: </h2>
-                        <Link href="/" className="underline">
-                            234234234234
-                        </Link>
-                    </div>
 
-                    <div className="flex items-center gap-2 mt-2">
-                        <LayoutDashboard size={20} />
-                        <h2 className="text-base font-medium">Payment status: </h2>
-                        <span>Paid</span>
-                    </div>
-                </CardContent>
-            </Card>
+                        <div className="flex items-center gap-2 mt-2">
+                            <LayoutDashboard size={20} />
+                            <h2 className="text-base font-medium">Payment status: </h2>
+                            <span>Paid</span>
+                        </div>
+                    </CardContent>
+                </Card>
+            )}
 
-            <Button asChild className="mt-6">
-                <Link href="/" className="flex items-center gap-2">
-                    <ArrowLeft size={20} className="text-white" />
-                    <span>Place another order</span>
-                </Link>
-            </Button>
+            {isOrderSuccess ? (
+                <Button asChild className="mt-6">
+                    <Link
+                        href={`/order-status/${params.orderId}`}
+                        className="flex items-center gap-2">
+                        <ArrowLeft size={20} className="text-white" />
+                        <span>Go to order status page</span>
+                    </Link>
+                </Button>
+            ) : (
+                <Button asChild className="mt-6">
+                    <Link href="/checkout" className="flex items-center gap-2">
+                        <ArrowLeft size={20} className="text-white" />
+                        <span>Go to checkout</span>
+                    </Link>
+                </Button>
+            )}
         </div>
     );
 };
